3e62b6db9593f9849ea30afb9698c43fe2526f0f
circa10a <caleblemoine@gmail.com>
2025-01-28 11:16:01 -0800

    switch voxpupuli/container-puppetserver to voxpupuli/puppetserver, suppress docker CLI hints
    Signed-off-by: circa10a <caleblemoine@gmail.com>

--- run_puppet_agent.sh
+++ run_puppet_agent.sh
@@ -2,12 +2,15 @@
 
 # Run puppet agent every 15 seconds
 INTERVAL=15
-CONTAINER_NAME=puppet
+CONTAINER_NAME=puppetserver
 MODULE_DIR=/etc/puppetlabs/code/environments/production/modules
 PUPPETFILE=/etc/puppetlabs/code/environments/production/Puppetfile
 
+# Suppress 'What's New' messages
+export DOCKER_CLI_HINTS=false
+
 # Check that container is running
-if [[ $(docker ps --filter "name=^/$CONTAINER_NAME$" --format '{{.Names}}') == $CONTAINER_NAME ]]; then
+if docker container inspect "$CONTAINER_NAME" > /dev/null 2>&1; then
   # Update package sources
   # Install deps
   docker exec -it $CONTAINER_NAME bash -c "r10k puppetfile install --verbose --puppetfile=$PUPPETFILE --moduledir=$MODULE_DIR"
@@ -16,4 +19,7 @@ if [[ $(docker ps --filter "name=^/$CONTAINER_NAME$" --format '{{.Names}}') == $
       docker exec -it $CONTAINER_NAME puppet agent -t
       sleep $INTERVAL
     done
+else
+  echo "'${CONTAINER_NAME}' container is not running. Run 'docker compose up' and try again"
+  exit 1
 fi
